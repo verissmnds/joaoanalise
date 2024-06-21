969358cec2688aadac5a5f13f395702693c9b3f9
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2024-06-20 23:12:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ df = pd.read_excel('posts1.xlsx')
 
 st.set_page_config(page_title="Análise do Instagram de João Campos", page_icon="📐", layout="wide")
 
-def main():
-    
-    st.title('Análise do Instagram de João Campos')
+st.title('Análise do Instagram de João Campos')
 
 df2 = df.sort_values(by='Post interaction rate', ascending=False).head(10)
 
-    st.header("As 10 publicações com maior taxa de engajamento:")
-    st.dataframe(df2)
+st.header("As 10 publicações com maior taxa de engajamento:")
+st.dataframe(df2)
 
-    st.header("Nuvem de palavras das legendas escritas pelo pré-candidato em suas publicações:")
-
-if __name__ == '__main__':
-    main()
+st.header("Nuvem de palavras das legendas escritas pelo pré-candidato em suas publicações:")
