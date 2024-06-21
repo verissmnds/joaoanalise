571fc75ba656ad6ea6af2fb57973f14963973e04
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2024-06-20 23:17:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ st.title('Análise do Instagram de João Campos')
 
 df2 = df.sort_values(by='Post interaction rate', ascending=False).head(10)
 
-st.header("As 10 publicações com maior taxa de engajamento:")
-st.dataframe(df2)
-
 st.header("Nuvem de palavras das legendas escritas pelo pré-candidato em suas publicações:")
 
 url_imagem = 'fotojoao.png'
 st.image(url_imagem)
+
+st.header("As 10 publicações com maior taxa de engajamento:")
+st.markdown("Na análise eu foquei apenas nas três primeiras publicações.")
+st.dataframe(df2)
