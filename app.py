e95973d5916cc3b74be2b31395c7611d99c0bdfc
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2024-06-20 22:55:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,32 +16,5 @@ st.dataframe(df)
 
 st.header("Nuvem de palavras das legendas escritas pelo pré-candidato em suas publicações:")
 
-stopwords = []
-with open('stopjoao.txt', 'r') as arquivo_de_stopwords:
-    for linha in arquivo_de_stopwords:
-        stopwords.append(linha.strip())  # Use strip para remover novas linhas e espaços em branco
-
-def limpar_texto(texto):
-    texto = re.sub(r'[^\w\s]', '', texto)  # Remove pontuação
-    texto = re.sub(r'\d+', '', texto)      # Remove números
-    texto = texto.lower()                  # Converte para minúsculas
-    return texto
-
-texto = joao_df['Message'].dropna().apply(limpar_texto)
-
-palavras = ' '.join(texto).split()
-
-palavras_sem_stopwords = [palavra for palavra in palavras if palavra not in stopwords]
-
-dicionario_palavras = Counter(palavras_sem_stopwords)
-
-wordcloud = WordCloud(width=800, height=400, background_color='white')
-wordcloud.generate_from_frequencies(dicionario_palavras)
-
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
-
 if __name__ == '__main__':
     main()
